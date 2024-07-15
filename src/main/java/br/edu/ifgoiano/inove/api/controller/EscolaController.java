76b79cfa3c0357fec0d07aa02d88c7f2f573fbef
Flávio Diniz de Sousa <102635915+Diniz-s-flavio@users.noreply.github.com>
2024-07-15 18:03:59 -0300

    15/07/24 - v2 - ResponseStatus em Escola

--- src/main/java/br/edu/ifgoiano/inove/api/controller/EscolaController.java
+++ src/main/java/br/edu/ifgoiano/inove/api/controller/EscolaController.java
@@ -1,8 +1,11 @@
 package br.edu.ifgoiano.inove.api.controller;
 
+import br.edu.ifgoiano.inove.domain.exceptions.EscolaNotFoundException;
 import br.edu.ifgoiano.inove.domain.model.Escola;
 import br.edu.ifgoiano.inove.domain.service.EscolaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,26 +23,36 @@ public class EscolaController {
     }
 
     @GetMapping("/{escolaId}")
-    public Escola findEscola(@PathVariable String escolaId){
+    public ResponseEntity<?> findEscola(@PathVariable String escolaId){
+        try {
+            Escola savedEscola = escolaService.findById(Long.parseLong(escolaId));
 
-        return escolaService.findById(Long.parseLong(escolaId));
+            return ResponseEntity.status(HttpStatus.OK).body(savedEscola);
+        }catch(EscolaNotFoundException ex){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+        }
     }
 
     @PostMapping
     public Escola addEscola(@RequestBody @Validated  Escola escola){
-
         return escolaService.add(escola);
     }
 
     @PutMapping("/{escolaId}")
-    public Escola UpdateEscola(@PathVariable String escolaId, @RequestBody @Validated Escola escola){
+    public ResponseEntity<?> UpdateEscola(@PathVariable String escolaId, @RequestBody @Validated Escola escola){
+        try {
+            Escola updatedEscola = escolaService.update(Long.parseLong(escolaId), escola);
 
-        return escolaService.update(Long.parseLong(escolaId), escola);
+            return ResponseEntity.status(HttpStatus.OK).body(updatedEscola);
+        }catch(EscolaNotFoundException ex){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+        }
     }
 
     @DeleteMapping("/{escolaId}")
-    public void deleteEscola(@PathVariable String escolaId){
+    public ResponseEntity deleteEscola(@PathVariable String escolaId){
 
         escolaService.deleteById(Long.parseLong(escolaId));
+        return ResponseEntity.noContent().build();
     }
 }
